c0f9f25b5540123a27a63629910ff000c7d4f4b5
Rajat Gupta <imrajatgupta@gmail.com>
2020-06-09 15:00:20 -0400

    Updates

---
+++ precache-manifest.854d7cb317063796e90b5bc09ad629cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d12e27e1e65b5a7f264",
+    "url": "/lights-out-web-app-game/static/css/main.5b5467a8.chunk.css"
+  },
+  {
+    "revision": "4d12e27e1e65b5a7f264",
+    "url": "/lights-out-web-app-game/static/js/main.4d12e27e.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/lights-out-web-app-game/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "f52c192585ffac42b58a",
+    "url": "/lights-out-web-app-game/static/js/runtime~main.f52c1925.js"
+  },
+  {
+    "revision": "4d8a4da565b578a4d7ce362105b82e6c",
+    "url": "/lights-out-web-app-game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/lights-out-web-app-game/precache-manifest.854d7cb317063796e90b5bc09ad629cb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/lights-out-web-app-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
